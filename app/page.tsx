b8e4f7b8c107713e9280e76743b5b206fb92d903
Dejvi31 <dejviepadejvi@gmail.com>
2024-01-11 14:59:28 +0100

    stilizuam search

--- app/page.tsx
+++ app/page.tsx
@@ -67,9 +67,11 @@ const Home = () => {
       <div className="mb-4">
         <input
           type="text"
-          placeholder="Search products ..."
+          name="search"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
+          className="block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          placeholder="Search for anything..."
         />
       </div>
       <Sorting handleSort={handleSort} sortBy={sortBy} sortOrder={sortOrder} />
