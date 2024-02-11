c74231cfd8451ccc7624d329fb7099675072fa5c
Dejvi31 <dejviepadejvi@gmail.com>
2024-02-11 15:54:42 +0100

    fixed No Product Found

--- app/components/forms/HeaderSearch.jsx
+++ app/components/forms/HeaderSearch.jsx
@@ -27,6 +27,11 @@ const HeaderSearch = ({
         className="block bg-white w-full max-w-md border border-slate-300 rounded-md mt-2 py-2 pl-9 pr-3 shadow-sm placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-gray-800 focus:ring-gray-800 focus:ring-1 sm:text-sm"
         placeholder={placeholder}
       />
+      {value.trim() !== "" && suggestions.length === 0 && (
+        <section className="absolute flex items-center justify-center top-10 z-10 bg-white border border-slate-300 mt-1 w-full max-w-md rounded-md shadow-lg">
+          <p className="text-sm"> No Product Found</p>
+        </section>
+      )}
       {suggestions.length > 0 && (
         <SuggestionsList
           suggestions={suggestions}
